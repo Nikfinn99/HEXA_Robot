fab61bb21a40da686633cb79d20facc4507c2b1f
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-04-17 20:52:37 +0200

    change from single leg to robot object

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,21 @@
 #include <FilterLinear.h>
 #include "point.h"
 #include "leg_objects.h"
+#include "robot.h"
 
 int x = 100;
 bool dir = true;
 int xmin = 0;
 int xmax = 180;
-int sx = 180, sy = 0, sz = -50;
+int sx = 100, sy = 0, sz = 30;
 int speed = 300;
+
+Robot robot(leg_fr, leg_r, leg_br, leg_fl, leg_l, leg_bl);
 
 void setup()
 {
   Serial.begin(115200);
-  delay(500);
+  delay(2000);
 
   Serial << "---SETUP-START---" << endl;
 
@@ -33,18 +36,15 @@ void setup()
   leg_l.setInitialPose();
   leg_bl.setInitialPose();
 
+  robot.setWalkParameters(-50, 50);
+  robot.setSpeed(speed, speed / 2);
+
   Serial << "---SETUP-END---" << endl;
 }
 
 void loop()
 {
-  // update all legs for interpolation
-  leg_fr.update();
-  leg_r.update();
-  leg_br.update();
-  leg_fl.update();
-  leg_l.update();
-  leg_bl.update();
+  robot.update();
 
   // parse incoming serial data
   while (Serial.available())
@@ -64,14 +64,22 @@ void loop()
     case 's': // leg movement speed
       speed = Serial.parseInt();
       break;
+    case 'r':
+      robot.setMode(WalkMode::RESET);
+      break;
+    case 'w':
+      robot.setMode(WalkMode::NORMAL);
+      break;
     }
   }
-  leg_br.setSpeed(speed);
 
-  Point p(sx, sy, sz);
-  leg_br.moveAbsolutePoint(p);
+  /*TEST LEG MOVEMENT*/
+  // leg_br.moveAbsoluteX(sx);
+  // leg_br.moveAbsoluteY(sy);
+  // leg_br.moveAbsoluteZ(sz);
 
-  // Point p(90, 90, 0);
+  /* INIT SERVOS */
+  // Point p(SERVO_ANGLE_1, SERVO_ANGLE_2, SERVO_ANGLE_3);
   // leg_fr.moveAngle(p, false);
   // leg_r.moveAngle(p, false);
   // leg_br.moveAngle(p, false);
