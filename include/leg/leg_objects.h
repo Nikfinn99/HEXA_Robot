449bd84af6133cb6bd47d934a7fbca854128b951
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-04-28 12:23:06 +0200

    create legs with correct angles of robot

--- include/leg/leg_objects.h
+++ include/leg/leg_objects.h
@@ -3,10 +3,10 @@
 #include "leg.h"
 #include "servo/servo_objects.h"
 
-Leg leg_fr(servos_fr, LEG_OFFS, LEG_LEN_1, LEG_LEN_2);
-Leg leg_r(servos_r, LEG_OFFS, LEG_LEN_1, LEG_LEN_2);
-Leg leg_br(servos_br, LEG_OFFS, LEG_LEN_1, LEG_LEN_2);
+Leg leg_fr(servos_fr, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, 40);
+Leg leg_r(servos_r, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, 0);
+Leg leg_br(servos_br, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, -40);
 
-Leg leg_fl(servos_fl, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, true);
-Leg leg_l(servos_l, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, true);
-Leg leg_bl(servos_bl, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, true);
+Leg leg_fl(servos_fl, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, 40, true);
+Leg leg_l(servos_l, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, 0, true);
+Leg leg_bl(servos_bl, LEG_OFFS, LEG_LEN_1, LEG_LEN_2, -40, true);
