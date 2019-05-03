8dbc1732089347b0df029f5c844d1680d0d35a06
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-05-03 10:14:25 +0200

    only set target if step is not running

--- src/robot/robot__movement.cpp
+++ src/robot/robot__movement.cpp
@@ -12,11 +12,16 @@ void Robot::walkSmooth(bool restart = false)
     if (restart)
     {
         step = 0;
+        step_running = false;
     }
 
-    switch (step)
+    if (!step_running)
     {
+        switch (step)
+        {
+        }
     }
+
     updateStep(&step_start, &step_running, &step, step_speed, 5, true);
 }
 
@@ -30,86 +35,90 @@ void Robot::walkNormal(bool restart = false)
     if (restart)
     {
         step = 0;
+        step_running = false;
     }
 
-    switch (step)
+    if (!step_running)
     {
-    case 0: /* OTHER legs DOWN */
+        switch (step)
+        {
+        case 0: /* OTHER legs DOWN */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* RLR DOWN */
-        m_leg_fr.moveRelZ(0, step_speed);
-        m_leg_l.moveRelZ(0, step_speed);
-        m_leg_br.moveRelZ(0, step_speed);
+            /* RLR DOWN */
+            m_leg_fr.moveRelZ(0, step_speed);
+            m_leg_l.moveRelZ(0, step_speed);
+            m_leg_br.moveRelZ(0, step_speed);
 
-        break;
+            break;
 
-    case 1: /* legs UP */
+        case 1: /* legs UP */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* LRL UP */
-        m_leg_fl.moveRelZ(m_walk_height, step_speed);
-        m_leg_r.moveRelZ(m_walk_height, step_speed);
-        m_leg_bl.moveRelZ(m_walk_height, step_speed);
+            /* LRL UP */
+            m_leg_fl.moveRelZ(m_walk_height, step_speed);
+            m_leg_r.moveRelZ(m_walk_height, step_speed);
+            m_leg_bl.moveRelZ(m_walk_height, step_speed);
 
-        break;
+            break;
 
-    case 2: /* legs FORWARD - OTHERS BACKWARD */
+        case 2: /* legs FORWARD - OTHERS BACKWARD */
 
-        step_speed = m_speed_slow; /* SLOW */
+            step_speed = m_speed_slow; /* SLOW */
 
-        /* LRL FRONT */
-        m_leg_fl.moveRelY(50, step_speed);
-        m_leg_r.moveRelY(50, step_speed);
-        m_leg_bl.moveRelY(50, step_speed);
+            /* LRL FRONT */
+            m_leg_fl.moveRelY(50, step_speed);
+            m_leg_r.moveRelY(50, step_speed);
+            m_leg_bl.moveRelY(50, step_speed);
 
-        /* RLR BACK */
-        m_leg_fr.moveRelY(-50, step_speed);
-        m_leg_l.moveRelY(-50, step_speed);
-        m_leg_br.moveRelY(-50, step_speed);
+            /* RLR BACK */
+            m_leg_fr.moveRelY(-50, step_speed);
+            m_leg_l.moveRelY(-50, step_speed);
+            m_leg_br.moveRelY(-50, step_speed);
 
-        break;
+            break;
 
-    case 3: /* legs DOWN */
+        case 3: /* legs DOWN */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* LRL DOWN */
-        m_leg_fl.moveRelZ(0, step_speed);
-        m_leg_r.moveRelZ(0, step_speed);
-        m_leg_bl.moveRelZ(0, step_speed);
+            /* LRL DOWN */
+            m_leg_fl.moveRelZ(0, step_speed);
+            m_leg_r.moveRelZ(0, step_speed);
+            m_leg_bl.moveRelZ(0, step_speed);
 
-        break;
+            break;
 
-    case 4: /* OTHER legs UP */
+        case 4: /* OTHER legs UP */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* RLR UP */
-        m_leg_fr.moveRelZ(m_walk_height, step_speed);
-        m_leg_l.moveRelZ(m_walk_height, step_speed);
-        m_leg_br.moveRelZ(m_walk_height, step_speed);
+            /* RLR UP */
+            m_leg_fr.moveRelZ(m_walk_height, step_speed);
+            m_leg_l.moveRelZ(m_walk_height, step_speed);
+            m_leg_br.moveRelZ(m_walk_height, step_speed);
 
-        break;
+            break;
 
-    case 5: /* legs BACKWARD - OTHER FORWARD */
+        case 5: /* legs BACKWARD - OTHER FORWARD */
 
-        step_speed = m_speed_slow; /* SLOW */
+            step_speed = m_speed_slow; /* SLOW */
 
-        /* LRL BACK */
-        m_leg_fl.moveRelY(-50, step_speed);
-        m_leg_r.moveRelY(-50, step_speed);
-        m_leg_bl.moveRelY(-50, step_speed);
+            /* LRL BACK */
+            m_leg_fl.moveRelY(-50, step_speed);
+            m_leg_r.moveRelY(-50, step_speed);
+            m_leg_bl.moveRelY(-50, step_speed);
 
-        /* RLR FRONT */
-        m_leg_fr.moveRelY(50, step_speed);
-        m_leg_l.moveRelY(50, step_speed);
-        m_leg_br.moveRelY(50, step_speed);
+            /* RLR FRONT */
+            m_leg_fr.moveRelY(50, step_speed);
+            m_leg_l.moveRelY(50, step_speed);
+            m_leg_br.moveRelY(50, step_speed);
 
-        break;
-    } /* end switch step */
+            break;
+        } /* end switch step */
+    }
 
     updateStep(&step_start, &step_running, &step, step_speed, 5, true);
 }
@@ -124,102 +133,106 @@ void Robot::resetLegs(bool restart = false)
     if (restart)
     {
         step = 0;
+        step_running = false;
     }
 
-    switch (step)
+    if (!step_running)
     {
-    case 0: /* ALL legs DOWN */
+        switch (step)
+        {
+        case 0: /* ALL legs DOWN */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* ALL DOWN */
-        m_leg_fl.moveRelZ(0, step_speed);
-        m_leg_l.moveRelZ(0, step_speed);
-        m_leg_bl.moveRelZ(0, step_speed);
-        m_leg_fr.moveRelZ(0, step_speed);
-        m_leg_r.moveRelZ(0, step_speed);
-        m_leg_br.moveRelZ(0, step_speed);
+            /* ALL DOWN */
+            m_leg_fl.moveRelZ(0, step_speed);
+            m_leg_l.moveRelZ(0, step_speed);
+            m_leg_bl.moveRelZ(0, step_speed);
+            m_leg_fr.moveRelZ(0, step_speed);
+            m_leg_r.moveRelZ(0, step_speed);
+            m_leg_br.moveRelZ(0, step_speed);
 
-        break;
+            break;
 
-    // EVERY SECOND LEG
-    case 1: /* legs UP */
+        // EVERY SECOND LEG
+        case 1: /* legs UP */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* LRL UP */
-        m_leg_fl.moveRelZ(m_walk_height, step_speed);
-        m_leg_r.moveRelZ(m_walk_height, step_speed);
-        m_leg_bl.moveRelZ(m_walk_height, step_speed);
+            /* LRL UP */
+            m_leg_fl.moveRelZ(m_walk_height, step_speed);
+            m_leg_r.moveRelZ(m_walk_height, step_speed);
+            m_leg_bl.moveRelZ(m_walk_height, step_speed);
 
-        break;
+            break;
 
-    case 2: /* RESET XY of legs */
+        case 2: /* RESET XY of legs */
 
-        step_speed = m_speed_slow; /* SLOW */
+            step_speed = m_speed_slow; /* SLOW */
 
-        /* LRL RESET */
-        m_leg_fl
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
-        m_leg_r
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
-        m_leg_bl
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
+            /* LRL RESET */
+            m_leg_fl
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
+            m_leg_r
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
+            m_leg_bl
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
 
-        break;
+            break;
 
-    case 3: /* legs DOWN */
+        case 3: /* legs DOWN */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* LRL DOWN */
-        m_leg_fl.moveRelZ(0, step_speed);
-        m_leg_r.moveRelZ(0, step_speed);
-        m_leg_bl.moveRelZ(0, step_speed);
+            /* LRL DOWN */
+            m_leg_fl.moveRelZ(0, step_speed);
+            m_leg_r.moveRelZ(0, step_speed);
+            m_leg_bl.moveRelZ(0, step_speed);
 
-        break;
+            break;
 
-    /* REMAINING LEGS */
-    case 4: /* OTHER legs UP */
+        /* REMAINING LEGS */
+        case 4: /* OTHER legs UP */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* RLR UP */
-        m_leg_fr.moveRelZ(m_walk_height, step_speed);
-        m_leg_l.moveRelZ(m_walk_height, step_speed);
-        m_leg_br.moveRelZ(m_walk_height, step_speed);
-        break;
+            /* RLR UP */
+            m_leg_fr.moveRelZ(m_walk_height, step_speed);
+            m_leg_l.moveRelZ(m_walk_height, step_speed);
+            m_leg_br.moveRelZ(m_walk_height, step_speed);
+            break;
 
-    case 5: /* RESET XY of OTHER legs */
+        case 5: /* RESET XY of OTHER legs */
 
-        step_speed = m_speed_slow; /* SLOW */
+            step_speed = m_speed_slow; /* SLOW */
 
-        /* RLR RESET */
-        m_leg_fr
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
-        m_leg_l
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
-        m_leg_br
-            .moveRelX(0, step_speed)
-            .moveRelY(0, step_speed);
-        break;
+            /* RLR RESET */
+            m_leg_fr
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
+            m_leg_l
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
+            m_leg_br
+                .moveRelX(0, step_speed)
+                .moveRelY(0, step_speed);
+            break;
 
-    case 6: /* OTHER legs DOWN */
+        case 6: /* OTHER legs DOWN */
 
-        step_speed = m_speed_fast; /* FAST */
+            step_speed = m_speed_fast; /* FAST */
 
-        /* RLR DOWN */
-        m_leg_fr.moveRelZ(0, step_speed);
-        m_leg_l.moveRelZ(0, step_speed);
-        m_leg_br.moveRelZ(0, step_speed);
-        break;
+            /* RLR DOWN */
+            m_leg_fr.moveRelZ(0, step_speed);
+            m_leg_l.moveRelZ(0, step_speed);
+            m_leg_br.moveRelZ(0, step_speed);
+            break;
 
-    } /* end switch step */
+        } /* end switch step */
+    }
 
     updateStep(&step_start, &step_running, &step, step_speed);
 }
