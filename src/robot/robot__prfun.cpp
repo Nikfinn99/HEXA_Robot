1814af0262befccfd726dd71f1b00144aae02ccf
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-05-08 17:53:35 +0200

    fix error: update step skipping step 0 step running was set to true immediately after reset of step to zero

--- src/robot/robot__prfun.cpp
+++ src/robot/robot__prfun.cpp
@@ -2,23 +2,28 @@
 
 void Robot::updateStep(unsigned long *step_start, bool *step_running, uint8_t *step, uint16_t p_time, uint8_t p_max_step, bool p_loop)
 {
-    if (*step > p_max_step)
-    {
-        if (p_loop)
-        {
-            (*step) = 0; /* reset to start step */
-        }
-        else
-        {
-            return; /* abort */
-        }
-    }
 
     if (!(*step_running)) /* not running */
     {
         (*step_running) = true; /* init running */
         (*step_start) = millis();
     }
+
+    if (*step > p_max_step)
+    {
+        if (p_loop)
+        {
+            (*step_running) = false;
+            (*step) = 0; /* reset to start step */
+        }
+        else
+        {
+            (*step_running) = false;
+            setMode(WalkMode::NONE);
+            return; /* abort */
+        }
+    }
+
     if (millis() > (*step_start) + p_time) /* finished */
     {
         (*step_running) = false;
