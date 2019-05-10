540ddd437272de5e324b043714ba75dc1cc2a7a5
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-05-10 22:35:03 +0200

    correct step update method

--- src/step/step__update.cpp
+++ src/step/step__update.cpp
@@ -2,27 +2,32 @@
 
 Step &Step::update()
 {
-    if (m_step > m_max_step)
-    {
-        if (m_loop)
-        {
-            m_step = 0; /* reset to start step */
-        }
-        else
-        {
-            return *this; /* abort */
-        }
-    }
-
     if (!m_step_running) /* not running */
     {
         m_step_running = true; /* init running */
         m_step_start = millis();
     }
-    if (millis() > m_step_start + m_step_speed) /* finished */
+
+    if (millis() > m_step_start + m_step_time) /* timeout -> next step */
     {
         m_step_running = false;
         m_step++; /* next step */
+    }
+
+    if (m_step > m_end_step)
+    {
+        if (m_loop)
+        {
+            m_finished = false;
+            m_step_running = false;
+            m_step = m_start_step; /* reset to start step */
+        }
+        else
+        {
+            m_step_running = false;
+            m_finished = true;
+            return; /* abort */
+        }
     }
 }
 
@@ -30,4 +35,5 @@ Step &Step::restart()
 {
     m_step = 0;
     m_step_running = false;
+    m_finished = false;
 }
