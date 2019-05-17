448333828251c6d31d5fb592c30b226476cec114
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-05-17 18:17:55 +0200

    smoother walking

--- src/robot/gaits/robot__walk_normal.cpp
+++ src/robot/gaits/robot__walk_normal.cpp
@@ -43,6 +43,17 @@ void Robot::walkNormal()
             m_leg_r.moveRelZ(m_walk_height, step.getTime());
             m_leg_bl.moveRelZ(m_walk_height, step.getTime());
 
+            /* RLR BACK */
+            m_leg_fr
+                .moveRelX(-p_fr.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_fr.y, m_speed_slow + 2 * m_speed_fast);
+            m_leg_l
+                .moveRelX(-p_l.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_l.y, m_speed_slow + 2 * m_speed_fast);
+            m_leg_br
+                .moveRelX(-p_br.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_br.y, m_speed_slow + 2 * m_speed_fast);
+
             break;
 
         case 2: /* legs FORWARD - OTHERS BACKWARD */
@@ -59,17 +70,6 @@ void Robot::walkNormal()
             m_leg_bl
                 .moveRelX(p_bl.x, step.getTime())
                 .moveRelY(p_bl.y, step.getTime());
-
-            /* RLR BACK */
-            m_leg_fr
-                .moveRelX(-p_fr.x, step.getTime())
-                .moveRelY(-p_fr.y, step.getTime());
-            m_leg_l
-                .moveRelX(-p_l.x, step.getTime())
-                .moveRelY(-p_l.y, step.getTime());
-            m_leg_br
-                .moveRelX(-p_br.x, step.getTime())
-                .moveRelY(-p_br.y, step.getTime());
 
             break;
 
@@ -93,22 +93,22 @@ void Robot::walkNormal()
             m_leg_l.moveRelZ(m_walk_height, step.getTime());
             m_leg_br.moveRelZ(m_walk_height, step.getTime());
 
+            /* LRL BACK */
+            m_leg_fl
+                .moveRelX(-p_fl.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_fl.y, m_speed_slow + 2 * m_speed_fast);
+            m_leg_r
+                .moveRelX(-p_r.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_r.y, m_speed_slow + 2 * m_speed_fast);
+            m_leg_bl
+                .moveRelX(-p_bl.x, m_speed_slow + 2 * m_speed_fast)
+                .moveRelY(-p_bl.y, m_speed_slow + 2 * m_speed_fast);
+
             break;
 
         case 5: /* legs BACKWARD - OTHER FORWARD */
 
             step.setTime(m_speed_slow); /* SLOW */
-
-            /* LRL BACK */
-            m_leg_fl
-                .moveRelX(-p_fl.x, step.getTime())
-                .moveRelY(-p_fl.y, step.getTime());
-            m_leg_r
-                .moveRelX(-p_r.x, step.getTime())
-                .moveRelY(-p_r.y, step.getTime());
-            m_leg_bl
-                .moveRelX(-p_bl.x, step.getTime())
-                .moveRelY(-p_bl.y, step.getTime());
 
             /* RLR FRONT */
             m_leg_fr
